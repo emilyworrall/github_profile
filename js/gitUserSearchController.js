d3bf06076cb61fd3a663bc1b762168d951713cb5
Emily Worrall <worrall.emily@gmail.com>
2015-10-20 11:14:32 +0100

    Implemented API

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,22 +1,13 @@
-githubUserSearch.controller('GitUserSearchController', [function() {
+githubUserSearch.controller('GitUserSearchController', ['$resource', function($resource) {
   var self = this;
 
-  self.doSearch = function() {
-    self.searchResult = {
-      'items': [
-        {
-          'login': 'tansaku',
-          'avatar_url': 'https://avatars.githubusercontent.com/u/30216?v=3',
-          'html_url': 'https://github.com/tansaku',
-        },
-        {
-          'login': 'stephenlloyd',
-          'avatar_url': 'https://avatars.githubusercontent.com/u/196474?v=3',
-          'html_url': 'https://github.com/stephenlloyd',
-        }
-      ]
-    };
-    console.log(self.searchTerm);
+  // var access_token = gitAccessToken;
+  // var url = 'https://api.github.com/users/' + $('input.username').val() + "?access_token=" + access_token;
+  var searchResource = $resource('https://api.github.com/search/users');
 
+  self.doSearch = function() {
+    self.searchResult = searchResource.get(
+      { q: self.searchTerm, access_token:gitAccessToken  }
+    );
   };
 }]);
