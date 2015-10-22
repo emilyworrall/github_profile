e7d1883cb8defe7c47643e00d2dada6380a4b25e
Emily Worrall <worrall.emily@gmail.com>
2015-10-22 15:58:05 +0100

    Three protractor tests passing, protractor tutorial mostly finished

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -1,7 +1,29 @@
 describe('Github Profile Finder', function() {
-  it('has a title', function() {
-    browser.get('http://localhost:8080');
 
+  var searchBox = element(by.model('searchCtrl.searchTerm'))
+  var searchButton = element(by.className('btn'))
+
+  beforeEach(function() {
+    browser.get('http://localhost:8080')
+  })
+
+  it('has a title', function() {
     expect(browser.getTitle()).toEqual('Github user search');
+  });
+
+  it('finds profiles', function() {
+    searchBox.sendKeys('spike');
+    searchButton.click();
+
+    var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
+    expect(profiles.get(0).getText()).toEqual('spike');
+  });
+
+  it('finds the last Spike', function() {
+    searchBox.sendKeys('spike');
+    searchButton.click();
+
+    var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
+    expect(profiles.last().getText()).toEqual('spikelynch');
   });
 });
